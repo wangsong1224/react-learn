47fb3271ef1a09f0d87304e80e45c9d3f873f921
王松 <572764113@qq.com>
2018-07-02 17:50:04 +0800

    增加文件

---
+++ .webpackrc.js
@@ -0,0 +1,15 @@
+const path = require("path")
+export default {
+    "extraBabelPlugins": [
+        [
+            "import", {
+                "libraryName": "antd",
+                "libraryDirectory": "es",
+                "style": "css"
+            }
+        ]
+    ],
+    "alias":{
+        "@":path.resolve(__dirname,'./src')
+    }
+}
